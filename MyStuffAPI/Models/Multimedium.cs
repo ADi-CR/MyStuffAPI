ea427ec4cf6b6199e52ae5a59ceee165f413b380
ADi <allandelgadoc@gmail.com>
2021-06-09 16:39:08 -0600

    Add project files.

---
+++ MyStuffAPI/Models/Multimedium.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MyStuffAPI.Models
+{
+    public partial class Multimedium
+    {
+        public int MultimediaId { get; set; }
+        public string MultimediaUri { get; set; }
+        public int ItemId { get; set; }
+
+        public virtual Item Item { get; set; }
+    }
+}
